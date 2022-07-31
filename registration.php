2b3feb502e371ecdfa1dc70903298a0838532d7b
EvgenyRomanov <e.romanov93@yandex.ru>
2022-07-31 17:51:11 +0300

    module7-task1

---
+++ registration.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'config.php';
+require_once 'helpers.php';
+require_once 'functions.php';
+
+
+$is_auth = 0;
+$title = "Регистрация";
+$class_main = "page__main--registration";
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $required = ['email', 'password', 'login', 'password-repeat'];              // Обазятельные для заполнения поля
+    $form = $_POST;
+
+    foreach ($required as $field) {
+        if (empty($form[$field])) {
+            if (strpos($field, "email")) {
+                $errors[$field] = "Электронная почта.<br>Это поле должно быть заполнено.";
+            } elseif ($field === 'password') {
+                $errors[$field] = "Пароль.<br>Это поле должно быть заполнено.";
+            } elseif ($field === 'login') {
+                $errors[$field] = "Логин.<br>Это поле должно быть заполнено.";
+            } elseif ($field === 'password-repeat') {
+                $errors[$field] = "Повтор пароля.<br>Это поле должно быть заполнено.";
+            }
+        }
+    }
+
+    if (filter_var($form['email'], FILTER_VALIDATE_EMAIL) === false) {
+        $errors['email'] = 'Некорретный email';
+    }
+
+    $errors['userpic-file'] = validate_avatar('userpic-file');
+    $errors = array_filter($errors);
+
+    if (empty($errors)) {
+
+        if (!empty($_FILES['userpic-file']['name'])) {
+            $file_from = $_FILES['userpic-file']['tmp_name'];
+            $file_to = __DIR__ . '/uploads/' . $_FILES['userpic-file']['name'];
+            move_uploaded_file($file_from, $file_to);
+            $form['userpic-file'] = '/uploads/' . $_FILES['userpic-file']['name'];
+        }
+
+        if (add_user($link, $errors, $form)) {
+            header("Location: /");
+            exit();
+        }
+    }
+
+}
+
+$main = include_template("main_reg.php", [
+    "errors" => $errors,
+]);
+
+
+$layout_content = include_template("layout.php", [
+    "is_auth" => $is_auth,
+    "title" => $title,
+    "main" => $main,
+    "class_main" => $class_main
+]);
+
+print($layout_content);
