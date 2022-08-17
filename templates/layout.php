56919a3dfe63aa6498a5a8e847f44a83ea4ab2a0
EvgenyRomanov <e.romanov93@yandex.ru>
2022-08-17 17:58:20 +0300

    module8-task2 скорректировал основной лейаут

--- templates/layout.php
+++ templates/layout.php
@@ -15,7 +15,7 @@
 <header class="header">
     <div class="header__wrapper container">
         <div class="header__logo-wrapper">
-            <a class="header__logo-link" href="main.html">
+            <a class="header__logo-link" href="index.php">
                 <img class="header__logo" src="img/logo.svg" alt="Логотип readme" width="128" height="24">
             </a>
             <p class="header__topic">
@@ -38,12 +38,12 @@
             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
-                        <a class="header__page-link <?php echo end(explode('/', $_SERVER['REQUEST_URI'])) === 'popular.php' ? 'header__page-link--active' : '';?>" href="popular.php" title="Популярный контент">
+                        <a class="header__page-link <?php echo strpos($_SERVER['REQUEST_URI'], 'popular.php') !== false ? 'header__page-link--active' : '';?>" href="popular.php" title="Популярный контент">
                             <span class="visually-hidden">Популярный контент</span>
                         </a>
                     </li>
                     <li class="header__my-page header__my-page--feed">
-                        <a class="header__page-link <?php echo end(explode('/', $_SERVER['REQUEST_URI'])) === 'feed.php' ? 'header__page-link--active' : '';?>" href="feed.php" title="Моя лента">
+                        <a class="header__page-link <?php echo strpos($_SERVER['REQUEST_URI'], 'feed.php') !== false ? 'header__page-link--active' : '';?>" href="feed.php" title="Моя лента">
                             <span class="visually-hidden">Моя лента</span>
                         </a>
                     </li>
